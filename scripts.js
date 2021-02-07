987035dbf6ef2df8d142a56b739c6479fc7b1e11
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-06 20:26:22 -0500

    Fixing transform glitch at 12 o'clock

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const hands = document.querySelectorAll('.hand');
 
 function setDate() {
-  const now = new Date();
+  const now = new Date(); 
+
   const seconds = now.getSeconds();
   const secondDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondDegrees}deg)`;
@@ -15,6 +17,15 @@ function setDate() {
   const hours = now.getHours();
   const hourDegrees = ((hours / 12) * 360) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  // remove glitch at 12 o'clock 
+  if (secondDegrees === 90) {
+    hands.forEach(hand => hand.style.transition = 'none')
+  } else {
+    // reset the inline style to return to the external styles
+    hands.forEach(hand => hand.style.transition = "" )
+  }
+
 }
 
 setInterval(setDate, 1000);
